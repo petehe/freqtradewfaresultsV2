4612af1427fbda69e86c312bc0982bf863fb46f2
petehe <petehe@gmail.com>
2021-12-02 20:24:56 +1100

    remove up summary file processing, only retain db processing

--- wfaresults/config.py
+++ wfaresults/config.py
@@ -1,15 +1,8 @@
-# FILE OR DB option
-# FILE to send wfa records into summary file, DB to send wfa records to database
-DB_OR_FILE = "DB"
-
 # additional parameters to insert along with the records
 ISPeriods = 6
 
 # the location of incoming result files
 wfaresultfolder = "resultfiles"
-
-# If use the file option, the location of summary files
-wfasummaryfolder = "wfasummary_temp"
 
 # If use the db option, the location of sqlite db
 wfadbpath = "db/wfaresults_defaultspace.sqlite"
--- wfaresults/main.py
+++ wfaresults/main.py
@@ -5,12 +5,8 @@ import config as cfg
 
 
 def main():
-    if cfg.DB_OR_FILE == "FILE":
-        summaryfile = wfasummaryfile(cfg.wfaresultfolder, cfg.wfasummaryfolder)
-        summaryfile.writesummaryfile()
-    elif cfg.DB_OR_FILE == "DB":
-        db = wfadb(cfg.wfaresultfolder, cfg.wfadbpath)
-        db.processresultfolder()
+    db = wfadb(cfg.wfaresultfolder, cfg.wfadbpath)
+    db.processresultfolder()
 
 
 if __name__ == "__main__":
