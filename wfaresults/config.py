6553972b4ca852f383a774ad729d4f4ab68baba2
petehe <petehe@gmail.com>
2021-12-30 07:51:10 +1100

    add results for BB_RPB_TSL_BI

--- wfaresults/config.py
+++ wfaresults/config.py
@@ -6,5 +6,5 @@ else:
     resultfolder = "wfaresultfiles"
 
 # If use the db option, the location of sqlite db
-wfadbpath = "db/wfaresultsV2.sqlite"
+wfadbpath = "db/wfaresultsV2_12.sqlite"
 # wfadbpath = "db/wfaresultstest.sqlite"
